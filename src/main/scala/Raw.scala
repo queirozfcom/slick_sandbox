33b361c8af794fb12853a10fa1da624f72ff85f8
felipe <falmeida1988@gmail.com>
2015-09-19 01:36:15 -0300

    more examples

--- src/main/scala/Raw.scala
+++ src/main/scala/Raw.scala
@@ -50,27 +50,27 @@ object Raw {
     val q = sqlu"insert into users(name,password) values ('john','mypasswd')"
 
     Await.result(
-      db.run(q).map{ res =>
+      db.run(q).map { res =>
         // prints the number of affected rows
         // which amount to 1 if the insert was successful
         println(res)
-      } ,timeout)
+      }, timeout)
 
   }
 
-  def update():Unit = {
+  def update(): Unit = {
     val q = sqlu"update users u set u.name='mary' where u.name = 'john'"
 
     Await.result(
-      db.run(q).map{ res =>
+      db.run(q).map { res =>
         // again, the number of affected rows
         println(res)
-      } ,timeout)
+      }, timeout)
 
   }
 
 
-  def errorHandling():Unit = {
+  def errorHandling(): Unit = {
     val id = "1"
     val name = "peter"
     val password = "passwd"
@@ -78,13 +78,42 @@ object Raw {
     val q = sqlu"insert into users(id,name,password) values($id,$name,$password)"
 
     Await.result(db.run(q).map { res =>
-      res match{
+      res match {
         case 1 => println("all good")
         case 0 => println("couldn't add record")
       }
-    }.recover{
-      case e:Exception =>println("Caught exception: "+e.getMessage)
+    }.recover {
+      case e: Exception => println("Caught exception: " + e.getMessage)
     }, timeout)
+  }
+
+  def transaction(): Unit = {
+
+    // insert or update, depending on whether that user already exists
+
+    val q = sql"select name from users where name='john'".as[String]
+
+    val qInsert = sqlu"insert into users(name,email,password) values('john','john@example.com','somepasswd')"
+
+    val qUpdate = sqlu"update users set email='john@example.com' where name='john'"
+
+    // this needs to be executed in a transaction
+    // because it might get written to after we run select
+    val actions = for {
+      exists <- q
+      affectedRows <- if (exists.isEmpty) qInsert else qUpdate
+    } yield affectedRows
+
+    Await.result(db.run(actions.transactionally).map { affectedRows =>
+      affectedRows match {
+        case 1 => println("all ok!")
+        case 0 => println("no rows were affected! maybe there was an error")
+      }
+    }.recover {
+      case e: Exception => println("Caught exception: " + e.getMessage)
+    }, timeout)
+
+
   }
 
 }
